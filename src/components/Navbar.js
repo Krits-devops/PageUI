edee1b9abd6a3f154b978aab8f120728f96d8804
Krits-devops <kritagyarm9@gmail.com>
2024-12-28 20:51:19 -0500

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../assets/Logo.svg'
+
+function Navbar(props) {
+
+    let isLoggedIn = props.isLoggedIn;
+    let setIsLoggedIn = props.isLoggedIn;
+
+  return (
+    <div className='flex justify-evenly'>
+        <Link to= '/'>
+           <img src={Logo} width={160} height={32} loading='lazy'/>
+        </Link>
+
+        <nav>
+            <ul className='flex gap-3' >
+                <li>fver
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/'>About</Link>
+                </li>
+                <li>
+                    <Link to='/'>Contact</Link>
+                </li>
+            </ul>
+        </nav>
+
+        {/* Login, Signup, Logout, Dashboard */}
+
+        <div className='flex ml-5 mr-3 gap-3'>
+            { !isLoggedIn && 
+                 <Link to= '/login'>
+                    <button>
+                        Login
+                    </button>
+                </Link>
+            }
+               
+            { !isLoggedIn &&
+                <Link to= '/signup'>
+                    <button>
+                        Signup
+                    </button>
+                </Link>
+            }
+            { isLoggedIn &&
+                <Link to= '/'>
+                    <button>
+                        Log Out
+                    </button>
+                </Link>
+            }
+            { isLoggedIn &&
+                <Link to= '/dashboard'>
+                    <button>
+                        Dashboard
+                    </button>
+                </Link>
+            }
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
